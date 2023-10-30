d8dce69768254c6a2b71cc3ddcb4bb5744507186
luan.soares.ext <luan.soares.ext@infracommerce.com.br>
2023-10-29 22:16:32 -0300

    [no-task] feat: Create casts timestamp.

---
+++ app/Casts/BirthdayCast.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Casts;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class BirthdayCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return Carbon::createFromFormat("Y-m-d", $value)->format("d/m/Y");
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return Carbon::createFromFormat("d/m/Y", $value)->format("Y-m-d");
+    }
+}
